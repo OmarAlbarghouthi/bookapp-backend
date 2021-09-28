93f9dcf4d47536c4dc156ac6e019ca050b1e5970
OmarAlbarghouthi <omar.qb8077@gmail.com>
2021-09-28 21:55:47 +0300

    backend done

--- server.js
+++ server.js
@@ -16,6 +16,37 @@ server.get('/books',getBookRouter)
 
 server.get('/', homeHandler);
 
+server.post('/addBooks',addBookHandler)
+server.delete('/deleteBooks',removeBookHandler)
+
+async function addBookHandler(req,res){
+    let {  email ,title, description,status } = req.body;
+    console.log(req.body);
+    await bookModel.create({
+       email,
+       title,
+       description,
+       status
+    })
+    await bookModel.find({email:email}, function(error,bookData){
+        if(error){console.log('error in getting data ' , error);}
+        else{
+            console.log(email);
+            res.send(bookData) 
+        
+        }
+    })
+}
+function removeBookHandler(req, res) {
+    let bookID = req.query.bookID;
+    let email = req.query.email;
+    bookModel.deleteOne({ _id: bookID }).then(() => {
+        bookModel.find({email:email}, function(error,bookData){
+            if(error){console.log('error in getting data ' , error);}
+            else{res.send(bookData)}
+        })
+    })
+}
 
 function homeHandler(req, res) {
     res.send('Everything is good')
